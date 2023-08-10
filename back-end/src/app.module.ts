76eff36fdc10138c99b7dc9d503851cc3dbbf92f
VuTran2601 <nguyenvutran2601@gmail.com>
2023-08-10 16:42:34 +0700

    Change config of database (using Environment Variable)

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -8,18 +8,23 @@ import { Seat } from './typeorm/entities/Seat';
 import { Ticket } from './typeorm/entities/Ticket';
 import { Seatmap } from './typeorm/entities/Seatmap';
 import { EventsBookingModule } from './events_booking/events_booking.module';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.DB_HOSTNAME)
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'config.env',
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOSTNAME,
-      port: parseInt(process.env.DB_PORT),
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: process.env.DB_DBNAME,
       entities: [Event, Customer, Seat, Ticket, Seatmap],
-      synchronize: true,
     }),
     EventsBookingModule,
   ],
