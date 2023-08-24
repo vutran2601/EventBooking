225fa926a3f69ed4afdf4cc80edbb55119d07ca2
VuTran2601 <nguyenvutran2601@gmail.com>
2023-08-24 10:14:52 +0700

    Update customers.service.ts

--- back-end/src/events_booking/service/customers/customers.service.ts
+++ back-end/src/events_booking/service/customers/customers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Customer } from 'src/typeorm/entities/Customer';
+import { Customer } from '../../../typeorm/entities/Customer'
 
 interface CustomerInfo {
   fullName: string;
