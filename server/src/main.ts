7b8289948bffba9ce891b52d9983ec5198f0347c
VuTran2601 <nguyenvutran2601@gmail.com>
2023-08-25 23:08:39 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://event-booking-client.vercel.app', // Replace with your client's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
