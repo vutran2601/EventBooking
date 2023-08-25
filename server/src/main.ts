87c34fd8abe384cc537f4daa6219ed1ab36d6ed1
VuTran2601 <nguyenvutran2601@gmail.com>
2023-08-25 18:58:35 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3001',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
