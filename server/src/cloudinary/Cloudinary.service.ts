0f8676d63763b57a4c9d0325222985502d7ce2c7
VuTran2601 <nguyenvutran2601@gmail.com>
2023-08-25 19:09:31 +0700

    Update import

--- server/src/cloudinary/Cloudinary.service.ts
+++ server/src/cloudinary/Cloudinary.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryResponse } from './cloudinary-response';
+import { CloudinaryResponse } from './Cloudinary-response';
 const streamifier = require('streamifier');
 
 @Injectable()
