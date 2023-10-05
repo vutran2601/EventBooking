69841713a1f1d71d3dd4920fc56c26d92c675997
Vu Tran Nguyen <vu.tran2601@hcmut.edu.vn>
2023-10-05 11:27:32 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
