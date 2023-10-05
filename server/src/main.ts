f579678e0349d05c23af16cda1a6d9f07841ab7c
Vu Tran Nguyen <vu.tran2601@hcmut.edu.vn>
2023-10-05 12:26:10 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://event-booking-client.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
