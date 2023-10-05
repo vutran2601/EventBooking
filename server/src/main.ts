2e92dce9c26c4f76d041594883ecaafd411081a9
Vu Tran Nguyen <vu.tran2601@hcmut.edu.vn>
2023-10-05 17:20:19 +0700

    Update CORS settings

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ['https://event-booking-client.vercel.app', 'http://localhost:3001'],
+        methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+        credentials: true,
+        allowedHeaders:
+        'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    });
+    await app.listen(3000);
 }
 bootstrap();
