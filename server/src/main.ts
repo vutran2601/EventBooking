e31de60045c006d8bfda1885ab6357b56bbd4ff0
Vu Tran Nguyen <vu.tran2601@hcmut.edu.vn>
2023-10-05 16:44:51 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3001', // The origin that is allowed to access the resource
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // The methods allowed when accessing the resource
+    allowedHeaders: 'Content-Type, Accept', // The headers that can be used when making the actual request
+  });  
   await app.listen(3000);
 }
 bootstrap();
